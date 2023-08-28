2838a7c2a3a7f6288c3dbee1ff48b2ea3572659f
Himanshu Negi <hnegi.7528@gmail.com>
2023-08-28 17:38:19 +0530

    caching feature added

--- popup.js
+++ popup.js
@@ -1,70 +1,117 @@
-const summarizeButton= document.querySelector(".summarizeBtn");
-const summaryArea= document.querySelector(".summary");
+const summarizeButton = document.querySelector(".summarizeBtn");
+const summaryArea = document.querySelector(".summary");
 const loader = document.getElementById("loader");
+const copyButton = document.querySelector('.copyButton');
+copyButton.classList.remove("active");
 
-
-
-summarizeButton.addEventListener("click", async ()=>{
-    
+summarizeButton.addEventListener("click", async () => {
     loader.classList.remove("hidden");
-    const summaryType= document.getElementById("summaryType").value;
-    let tab = await chrome.tabs.query({active: true, currentWindow: true});
-    chrome.scripting.executeScript({
-        target: { tabId: tab[0].id },
-        function: summaryFun,
-        args: [{ summaryType: summaryType }]
-    },
-    async (injectionResults)=>{
-        const [data]= injectionResults;
-        if(data.result){
-            loader.classList.add("hidden");
-            summaryArea.innerText=data.result;
-            
-        }
-    });
+    copyButton.classList.remove("active");
+    copyButton.innerHTML = "Copy";
+    summaryArea.classList.add("hidden");
+    const summaryType = document.getElementById("summaryType").value;
+    const tab = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    const tabId = tab[0].id;
+    const tabUrl = tab[0].url;
+    const cacheKey =  generateCacheKey(tabId, tabUrl, summaryType);
+    const cachedSummary = await getSummaryFromCache(cacheKey);
+
+    if (cachedSummary) {
+        loader.classList.add("hidden");
+        summaryArea.classList.remove("hidden");
+        console.log("Using cached summary");
+        summaryArea.innerHTML = cachedSummary;
+    } else {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            function: summaryFun,
+            args: [{ summaryType: summaryType, tabId: tabId }]
+        }, async (injectionResults) => {
+            const [data] = injectionResults;
+            if (data.result) {
+                loader.classList.add("hidden");
+                console.log("Generated summary:", data.result);
+                summaryArea.innerHTML = data.result;
+                await saveSummaryToCache(tabId, tabUrl, summaryType, data.result);
+            }
+        });
+    }
 });
 
-async function summaryFun(args){
-    try{
+function generateCacheKey(tabId, tabUrl, summaryType) {
+    return `summary_${tabId}_${tabUrl}_${summaryType}`;
+}
 
-        const headingsAndParagraphs = await Array.from(document.querySelectorAll('h1, h2, h3, p'));
+async function getSummaryFromCache(cacheKey) {
+    return new Promise((resolve) => {
+        chrome.storage.local.get([cacheKey], (result) => {
+            resolve(result[cacheKey]);
+        });
+    });
+}
+
+async function saveSummaryToCache(tabId, tabUrl, summaryType, summaryContent) {
+    const cacheKey = generateCacheKey(tabId, tabUrl, summaryType);
+    const cacheData = { [cacheKey]: summaryContent };
+    chrome.storage.local.set(cacheData);
+    console.log("Saved summary to cache");
+}
+
+async function summaryFun(args) {
+    try {
+        const headingsAndParagraphs = Array.from(document.querySelectorAll('h1, h2, h3, p'));
         const webpageText = headingsAndParagraphs.map(element => element.textContent).join('\n ');
-        console.log("WebPageText::", webpageText);
 
-        //summary function
         async function summarizeText(msgContent, summaryType) {
             const API_KEY = "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";
-          
-          const language = "English";
-          const prompt = summaryType === 'general'
-            ? `Instructions: Summarize this content in brief in ${language} language with correct grammar. Your response should provide an overview of the information presented in the content and should not include personal comments.`
-            : "Summary Mode: Bullet-Points\n\nInstructions: Summarize the content in bullet-point format";
-          
+            const language = "English";
+            const prompt = summaryType === 'general'
+                ? `Instructions: Summarize this content in brief in ${language} language with correct grammar...`
+                : "Summary Mode: Bullet-Points\n\nInstructions: Summarize the content in bullet-point format";
             const apiRequestBody = {
-              "model": "gpt-3.5-turbo",
-              "messages": [
-                  { "role": "system", "content": prompt },
-                  { "role": "user", "content": msgContent }
-              ],
-              max_tokens: 150
-          };
-          
-          // Return the fetch promise to handle the asynchronous response
-          return fetch("https://api.openai.com/v1/chat/completions", {
-              method: "POST",
-              headers: {
-                  "Authorization": "Bearer " + API_KEY,
-                  "Content-Type": "application/json"
-              },
-              body: JSON.stringify(apiRequestBody)
-          })
-          .then(response => response.json())
-          .then(data => data.choices[0].message.content);
-          }
-          
-        return await summarizeText(webpageText,args.summaryType)
-   
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                    { "role": "system", "content": prompt },
+                    { "role": "user", "content": msgContent }
+                ],
+                max_tokens: 150
+            };
+
+            const response = await fetch("https://api.openai.com/v1/chat/completions", {
+                method: "POST",
+                headers: {
+                    "Authorization": "Bearer " + API_KEY,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(apiRequestBody)
+            });
+
+            const data = await response.json();
+            return data.choices[0].message.content;
+        }
+       
+        if(args.summaryType === "general") {return summarizeText(webpageText, args.summaryType);}
+            const rawSummaryContent = await summarizeText(webpageText, args.summaryType);
+            const bulletPoints = rawSummaryContent.split('-').filter(item => item.trim() !== "");
+            const orderedList = bulletPoints.map((point, index) => `<li>${point}</li>`).join("\n");
+            const formattedSummary = `<ol>${orderedList}</ol>`;
+            return formattedSummary.trim();
+    
+        
+    } catch (e) {
+        console.log("Error:", e);
     }
-    catch(e){console.log("why error",e);}
 }
+
+
+
+copyButton.addEventListener('click', function() {
+  copyButton.classList.add('active');
+  if (this.innerHTML = "Copy") {
+    this.innerHTML = "Copied!";
+    navigator.clipboard.writeText(summaryArea.innerText)
+    console.log(summaryArea.innerText);
+  }
+})
 
